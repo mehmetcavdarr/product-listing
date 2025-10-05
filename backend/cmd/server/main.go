b8c2ecc92ddf65993153f95cb7f598b330c8a138
Mehmet Çavdar <mehmetcvdr4567@gmail.com>
2025-10-05 14:12:23 +0300

    [fix]

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -14,7 +14,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "*",
 		AllowMethods: "GET,HEAD,OPTIONS",
 		AllowHeaders: "*",
 	}))
